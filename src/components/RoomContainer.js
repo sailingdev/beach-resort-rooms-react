b1af2b78fe3a193a70657bfbd88764e89f5f3b54
sailingdev <sailingdev123@gmail.com>
2021-06-13 19:09:27 +0800

    v1.0

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import RoomFilter from './RoomFilter';
 import RoomList from './RoomList';
-import { withRoomConsumer, RoomConsumer } from '../context';
+import { withRoomConsumer } from '../context';
 import Loading from './Loading';
 
 function RoomContainer({ context }) {
